391b8d6c7f8504cab16775926c45c95853f061ba
郭玉朝 <2788828128@qq.com>
2018-04-25 23:18:04 +0800

    日志类封装完成

---
+++ extends/tool/log/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: 郭玉朝
+ * CreateTime: 2018/4/25 下午7:04
+ * Description:
+ */
+namespace Tool\Log;
+class Config
+{
+    const LOG_PATH = PUBLIC_PATH . '/../runtime'; // 日志目录
+    const TIME_ZONE = 'Asia/Shanghai'; // 模式时区为上海
+    const LOG_DEBUG = "DEBUG"; // 调试模式
+    const LOG_ERROR = "ERROR"; // 错误
+    const LOG_TRACE = "TRACE"; // 追踪模式
+}
+
